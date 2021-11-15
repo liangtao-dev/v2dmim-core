2353801289b44c1d176d8a12c67568ce0ad3166f
Iceㄨ涛 <liangtao.gz@foxmail.com>
2021-11-15 11:09:15 +0800

    新增 Error 结构体；

---
+++ src/struct/Error.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+// +----------------------------------------------------------------------
+// | CodeEngine
+// +----------------------------------------------------------------------
+// | Copyright 艾邦
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: TaoGe <liangtao.gz@foxmail.com>
+// +----------------------------------------------------------------------
+// | Version: 2.0 2021/11/15 11:03
+// +----------------------------------------------------------------------
+namespace V2dmIM\Core\struct;
+
+use V2dmIM\Core\Struct;
+
+class Error extends Struct
+{
+    /**
+     * 错误代码
+     * @var int
+     */
+    private int $code = 0;
+
+    /**
+     * 错误消息
+     * @var ?string
+     */
+    private ?string $message = null;
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param int $code
+     */
+    public function setCode(int $code): void
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string|null $message
+     */
+    public function setMessage(?string $message): void
+    {
+        $this->message = $message;
+    }
+
+
+}
